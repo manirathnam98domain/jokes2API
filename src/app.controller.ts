ad1fba112ae8cfb27ea532b5c78b2d6b7fb32929
manirathnam98domain <manirathnam98domain@gmail.com>
2025-06-10 18:07:58 +0530

    inital commit

---
+++ src/app.controller.ts
@@ -0,0 +1,18 @@
+import { Controller, Get, Post } from '@nestjs/common';
+import { AppService } from './app.service';
+
+@Controller()
+export class AppController {
+  constructor(private readonly appService: AppService) {}
+
+  @Get()
+  getHello(): any {
+    return this.appService.getHello();
+  }
+
+
+  @Post()
+  createNewPost():string{
+    return ""
+  }
+}
